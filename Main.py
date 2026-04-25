9ac6924072733415f05181d3d19924b1c4abe699
Chester_Pacana <pacanachester@gmail.com>
2026-04-25 22:09:49 +0800

    Update Main.py
    Made Student Its own file

--- Main.py
+++ Main.py
@@ -1,14 +1,5 @@
 import sys,time,os
-
-class Student: #Prints the name,age and course of students
-    def __init__(self,name,age,course):
-        self.name=name
-        self.age=age
-        self.course=course
-    def __str__(self):
-        return f"{self.name}, {self.age}, {self.course}"
-    def Info(self):
-        return str(self)
+from Student import Student
 
 class StudentSystem:
 
